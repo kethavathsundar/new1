51722907e1be4afc4caf46e661059dc1247dc84d
kethavathsundar <90022710+kethavathsundar@users.noreply.github.com>
2021-10-31 11:31:41 +0530

    Create 3.js

---
+++ 3.js
@@ -0,0 +1,17 @@
+/**
+ * @sundar Kethavath
+ */
+// should be undefined
+ var x;
+console.log(x);
+
+if(x==undefined){
+console.log(" x is undefined");
+}
+x=5;
+if(x==undefined){
+console.log("x is defined");
+}
+else {
+console.log(" x has been defined");
+}

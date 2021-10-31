9b89da2f78fb500e18bdbad9752be1479d9c0666
kethavathsundar <90022710+kethavathsundar@users.noreply.github.com>
2021-10-31 11:03:37 +0530

    Create 2.js

---
+++ 2.js
@@ -0,0 +1,11 @@
+var message="in global";
+console.log("in global: message="+message);
+var a=function (){
+var message="inside a";
+console.log("a: message="+message);
+b();
+}
+function b(){
+console.log("b: message="+message);
+}
+a();
